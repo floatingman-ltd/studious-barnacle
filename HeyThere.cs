feaa5e49e3c92cafa524bf5909407166ec7d5716
waltiam <2963975+waltiam@users.noreply.github.com>
2022-05-20 16:17:18 +0000

    🎉 other then some boiler plate

---
+++ HeyThere.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+
+namespace studious_barnacle;
+
+public class HeyThere
+{
+    public string HelloWorld => "Hello World!";
+
+    public string HelloAny(string name) => $"Hello {name}";
+
+#if DEBUG
+
+    [Fact]
+    public void HelloTest() => this.HelloWorld.Should().Be("Hello World!");
+
+    [Theory]
+    [InlineData("walt")]
+    [InlineData("Kimberly")]
+    public void HelloWalt(string name) => this.HelloAny(name).Should().Be($"Hello {name}");
+
+#endif
+}
